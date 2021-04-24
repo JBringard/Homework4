71e50506f6232d83e265956215a2176494df6b8b
gbringard <gbringard@ltu.edu>
2021-04-24 17:43:50 -0400

    Block comment

--- src/server.js
+++ src/server.js
@@ -1,3 +1,13 @@
+/*
+* Gerard Bringard
+* NodeJS Homework4
+*
+* This program utilizes websockets to have
+* a functioning autocomplete. On every
+* keystroke the user text is sent to the
+* backend, and the regex is used to search the
+* database. On a submit, whatever was in the text
+* field is added to the database. */
 require('dotenv').config();
 const Express = require('express');
 const Mongoose = require('mongoose');
